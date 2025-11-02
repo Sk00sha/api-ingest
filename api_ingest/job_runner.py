fa6b7aea55306204de53cfa6906ef19fd33f386f
filipwagner <filipwagner23@gmail.com>
2025-11-02 23:00:00 +0100

    rework for api-ingest pattern

--- api_ingest/job_runner.py
+++ api_ingest/job_runner.py
@@ -1,4 +1,3 @@
-import json
 import logging
 from api_ingest.app_context import AppContext, HistoryLoadConfig, Endpoint, AuthConfig, NotificationContext
 from api_ingest.api_client import ApiDataClient
@@ -17,10 +16,9 @@ class Runner:
         try:
             ctx = conf.load_app_context(self.path)
             api_client = ApiDataClient(ctx)
-            data = api_client.fetch_data()
-            print(f"Fetched {len(data)} records in total.")
+            api_client.fetch_data()
         except Exception as e:
-            logging.error(f"Job failed: {e}")
+            logging.error(f"Job failed : {e}")
             sn_ctx = NotificationContext(
                 base_url="https://instance.service-now.com",
                 username="api_user",
