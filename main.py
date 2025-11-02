d920483da999dde760f2eb32896e3a29388bcad5
Sk00sha <filipwagner23@gmail.com>
2025-11-02 15:10:03 +0100

    Refactor api-ingestion pattern

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
 from api_ingest.config_loader import ConfigLoader
-from api_ingest.api_client import MyApiClient
 from api_ingest.api_ingestor import ApiIngestor
-from api_ingest.loader import Loader
 
 
 def main():
